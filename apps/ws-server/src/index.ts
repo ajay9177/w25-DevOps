c3851ffdc85f4dde13e4e62a0980e392ab34b22d
ajay9177 <ajay098251@gmail.com>
2025-02-15 02:03:02 +0530

    Fixed CD

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -2,7 +2,7 @@ import { WebSocketServer } from "ws";
 import {client} from "@repo/db/client";
 const server=new WebSocketServer({
     port:3001
-});
+}); //
 server.on("connection",async (socket)=>{
     await client.user.create({
         data:{
